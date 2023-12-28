b3a33a0b886fadb701f28e6a4ee061cec0cd294f
diptanu <diptanuc@gmail.com>
2023-12-28 16:49:36 +0000

    Added the jina ai embedding model

---
+++ embedding-extractors/jina_base_en/jina_base_en.py
@@ -0,0 +1,32 @@
+from typing import List
+from indexify_extractor_sdk import (
+    ExtractorSchema,
+    EmbeddingSchema,
+    Content,
+)
+from indexify_extractor_sdk.base_embedding import (
+    BaseEmbeddingExtractor,
+    EmbeddingInputParams,
+)
+from transformers import AutoModel
+
+class JinaEmbeddingsBase(BaseEmbeddingExtractor):
+    def __init__(self):
+        super(JinaEmbeddingsBase, self).__init__(max_context_length=512)
+        self._model = AutoModel.from_pretrained('jinaai/jina-embeddings-v2-base-en', trust_remote_code=True)
+
+    def extract_embeddings(self, texts: List[str]) -> List[List[float]]:
+        return self._model.encode(texts).tolist()
+
+    def schemas(self) -> ExtractorSchema:
+        return ExtractorSchema(
+            features={
+                "embedding": EmbeddingSchema(distance_metric="cosine", dim=768)
+            },
+        )
+
+
+if __name__ == "__main__":
+    extractor = JinaEmbeddingsBase()
+    print(extractor.schemas())
+    print(extractor.extract([Content.from_text(text="Hello World")], EmbeddingInputParams()))
